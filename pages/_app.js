57ca74ce304f3fb34a1050369fa297a258b857ce
ggdaltoso <ggdaltoso@gmail.com>
2023-07-27 14:00:56 -0300

    feat: pega o pai de fundo pei pei

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import './style.css'
 import { ThemeProvider, GlobalStyle } from '@react95/core';
+import { createGlobalStyle } from '@xstyled/styled-components';
+
+const Wallpaper = createGlobalStyle`
+  body {
+    background: url(/image/wallpaper.jpg) no-repeat center center fixed;
+    background-size: cover;
+  }
+`;
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
       <GlobalStyle />
+      <Wallpaper />
       <Component {...pageProps} />
     </ThemeProvider>
   );
