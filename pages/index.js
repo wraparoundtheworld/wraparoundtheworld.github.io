6437070dd658cb2fad6013d02dc644c0b5973738
ggdaltoso <ggdaltoso@gmail.com>
2023-07-28 11:16:16 -0300

    fix: update img paths

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,24 @@
 import { useState, useCallback } from 'react';
 import Head from 'next/head';
-import { Modal, List, Frame, TaskBar, Fieldset, Input, Button} from "@react95/core";
+import { Modal, List, Frame, TaskBar, Fieldset, Input, Button } from "@react95/core";
 import styled from 'styled-components';
-import MonicaHenrique_pixelated from "../public/image/Monica_HenriquepxArt.png";
-import Heart from "../public/image/heart.gif"
-import Pix from "../public/image/qrcode-pix.png"
+
 import { Drvspace7, Issue, FilePin, Packager1, Mmsys112, Explorer108 } from "@react95/icons";
 
-const dataFormatada = function()
-{
+const dataFormatada = function () {
   let _second = 1000;
   let _minute = _second * 60;
   let _hour = _minute * 60;
   let _day = _hour * 24;
 
-  let distancia = Math.abs(new Date() - new Date(2024,3,13,18))
+  let distancia = Math.abs(new Date() - new Date(2024, 3, 13, 18))
 
   var d = Math.floor(distancia / _day);
   var h = Math.floor((distancia % _day) / _hour);
   var m = Math.floor((distancia % _hour) / _minute);
   var s = Math.floor((distancia % _minute) / _second);
 
-  return d + ' dias ' + h +' horas ' + m + ' minutos ' + s + ' segundos';
+  return d + ' dias ' + h + ' horas ' + m + ' minutos ' + s + ' segundos';
 };
 
 const dataRegressiva = dataFormatada();
@@ -36,12 +33,11 @@ const InsideModal = styled(Frame)`
   padding: 20px;
 `;
 
-
 export default function Home() {
 
   /* Spotify Modal */
   const [showSpotifyModal, setShowSpotifyModal] = useState(false);
-  
+
   const handleOpenSpotifyModal = useCallback(() => {
     setShowSpotifyModal(true);
   }, []);
@@ -107,11 +103,11 @@ export default function Home() {
   };
 
   /* Github */
-  const openGithub = function() {
+  const openGithub = function () {
     window.open("https://github.com/henriquespecian/casamento-nextjs");
   }
 
-  return (    
+  return (
     <div>
 
       <Head>
@@ -140,9 +136,9 @@ export default function Home() {
           <div>
             <br></br>
             <div>
-              <img src={Heart} className='heart'></img>
-              <h1 className='titulo' >Monica & Henrique</h1> 
-              <img src={Heart} className='heart'></img>
+              <img src="/image/heart.gif" className='heart'></img>
+              <h1 className='titulo' >Monica & Henrique</h1>
+              <img src="/image/heart.gif" className='heart'></img>
             </div>
             <br></br>
             <h2>São Paulo. 13 Abril 2024</h2>
@@ -150,13 +146,13 @@ export default function Home() {
             <br></br>
             <img
               className='image-center'
-              src={MonicaHenrique_pixelated}
-              alt="Monica & Henrique & Sophia"              
+              src="/image/Monica_HenriquepxArt.png"
+              alt="Monica & Henrique & Sophia"
             />
             <h2>Vamos nos casar!</h2>
             <p>
               O grande dia está chegando e não poderíamos estar mais animados para compartilhar com a nossa família e amigos um dos dias mais especiais das nossas vidas!
-            </p>          
+            </p>
             <br></br>
             <p>
               Queremos muito sua presença neste dia tão importante com muita festa, amor e carinho em uma comemoração que irá nos marcar para sempre!
@@ -164,16 +160,16 @@ export default function Home() {
             <br></br>
             <br></br>
             <h2>Faltam:</h2>
-            
+
             <br></br>
             <br></br>
             <h2>Endereco</h2>
-            <iframe 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.153255731116!2d-46.6552800236925!3d-23.526989660332237!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59a6f0437e37%3A0xf08fb1dfb1c91838!2sA%20Casa%20de%20Babette!5e0!3m2!1spt-BR!2sbr!4v1689711470236!5m2!1spt-BR!2sbr" 
-              width="400" 
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.153255731116!2d-46.6552800236925!3d-23.526989660332237!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59a6f0437e37%3A0xf08fb1dfb1c91838!2sA%20Casa%20de%20Babette!5e0!3m2!1spt-BR!2sbr!4v1689711470236!5m2!1spt-BR!2sbr"
+              width="400"
               height="300"
-              style={{border:0}} 
-              loading="lazy" 
+              style={{ border: 0 }}
+              loading="lazy"
               referrerPolicy="no-referrer-when-downgrade">
             </iframe>
             <p>Rua Anhanguera, 104, A Casa de Babette - Barra funda, São Paulo - SP</p>
@@ -185,124 +181,125 @@ export default function Home() {
             <p>Você pode fazer um pix direto pra gente</p>
             <br></br>
             <br></br>
-            <img
-              className='image-center'
-              src={Pix}
-              alt="QrCode do Pix"
-              href="https://nubank.com.br/pagar/48wth/mGWKfQeR5q"
-            />            
-            <a href="https://nubank.com.br/pagar/48wth/mGWKfQeR5q">Pague aqui</a>
+            <a href="https://nubank.com.br/pagar/48wth/mGWKfQeR5q">
+              <img
+                className='image-center'
+                src="/image/qrcode-pix.png"
+                alt="QrCode do Pix"
+                href="https://nubank.com.br/pagar/48wth/mGWKfQeR5q"
+              />
+              Pague aqui</a>
           </div>
         </InsideModal>
       </Modal>}
 
-  {/* Modal do spotify  */}
-  {showSpotifyModal && <Modal
-          title='Playlist do casamento.exe'
-          className='modal-spotify'
-          icon={<Mmsys112 variant="32x32_4" />}
-          closeModal={handleCloseSpotifyModal }
-          menu={[
-            {
-              name: "Options",
-              list: (
-                <List>
-                  <List.Item onClick={handleCloseSpotifyModal }>Close</List.Item>
-                </List>
-              ),
-            },
-          ]}>
-     <iframe 
-        src="https://open.spotify.com/embed/playlist/5eGxfvGynjkVO96JtlaBPE?utm_source=generator&theme=0" 
-        width="100%" 
-        height="100%" 
-        frameBorder="0" 
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-        loading="lazy"
-        title="Músicas para lembrar da Mo">
-      </iframe>
-  </Modal>
-  }
+      {/* Modal do spotify  */}
+      {showSpotifyModal && <Modal
+        title='Playlist do casamento.exe'
+        className='modal-spotify'
+        icon={<Mmsys112 variant="32x32_4" />}
+        closeModal={handleCloseSpotifyModal}
+        menu={[
+          {
+            name: "Options",
+            list: (
+              <List>
+                <List.Item onClick={handleCloseSpotifyModal}>Close</List.Item>
+              </List>
+            ),
+          },
+        ]}>
+        <iframe
+          src="https://open.spotify.com/embed/playlist/5eGxfvGynjkVO96JtlaBPE?utm_source=generator&theme=0"
+          width="100%"
+          height="100%"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+          title="Músicas para lembrar da Mo">
+        </iframe>
+      </Modal>
+      }
 
-  {/* Confirmação de presença  */}
-  {showConfirmacaoModal && <Modal
-    title='Confirmação de Presença'
-    icon={<FilePin variant="32x32_4" />}
-    closeModal={handleCloseConfirmacaoModal }
-    menu={[
-      {
-        name: "Options",
-        list: (
-          <List>
-            <List.Item onClick={handleCloseConfirmacaoModal }>Close</List.Item>
-          </List>
-        ),
-      },
-    ]}>
-      <Fieldset legend='Nome dos presentes'>
-        <div>
-          <label>Nome e sobrenome: </label>
-          <Input type='text' placeholder='Nome e sobrenome' required></Input>
-          <Button onClick={addInput}>Adicionar acompanhante</Button>
-        </div>
+      {/* Confirmação de presença  */}
+      {showConfirmacaoModal && <Modal
+        title='Confirmação de Presença'
+        icon={<FilePin variant="32x32_4" />}
+        closeModal={handleCloseConfirmacaoModal}
+        menu={[
+          {
+            name: "Options",
+            list: (
+              <List>
+                <List.Item onClick={handleCloseConfirmacaoModal}>Close</List.Item>
+              </List>
+            ),
+          },
+        ]}>
+        <Fieldset legend='Nome dos presentes'>
+          <div>
+            <label>Nome e sobrenome: </label>
+            <Input type='text' placeholder='Nome e sobrenome' required></Input>
+            <Button onClick={addInput}>Adicionar acompanhante</Button>
+          </div>
 
           {arr.map((item, i) => {
-          return (
-            <div>
-              <label>Nome e sobrenome: </label>
-              <Input
-                onChange={handleChange}
-                value={item.value}
-                id={i}
-                type={item.type}
-                size="40"
-              />
-            </div>
-          );
-        })}
-        
-      </Fieldset>
+            return (
+              <div>
+                <label>Nome e sobrenome: </label>
+                <Input
+                  onChange={handleChange}
+                  value={item.value}
+                  id={i}
+                  type={item.type}
+                  size="40"
+                />
+              </div>
+            );
+          })}
 
-      <div className='enviarCancelar'>
-        <Button className='botoes'>Enviar</Button>
-        <Button className='botoes'>Cancelar</Button>
-      </div>
-  </Modal>}
+        </Fieldset>
+
+        <div className='enviarCancelar'>
+          <Button className='botoes'>Enviar</Button>
+          <Button className='botoes'>Cancelar</Button>
+        </div>
+      </Modal>}
 
 
-  {/* Menu Iniciar  */}
-  <TaskBar
-    list={
-      <List>
+      {/* Menu Iniciar  */}
+      <TaskBar
+        list={
+          <List>
 
-        <List.Item
-          icon={<Drvspace7 variant="32x32_4" />}
-          onClick={handleOpenCasamentoModal}>
-          Casamento
-        </List.Item>
-        <List.Item
-          icon={<Mmsys112 variant="32x32_4" />}
-          onClick={handleOpenSpotifyModal}>
-          Playlist do casamento
-        </List.Item>
-        <List.Item
-          icon={<Packager1 variant="32x32_4" />}>
-          Lista de presentes
-        </List.Item>
-        <List.Item
-          icon={<FilePin variant="32x32_4" />}
-          onClick={handleOpenConfirmacaoModal}>
-          Confirmação de presença
-        </List.Item>
-        <List.Divider />
-        <List.Item
-          icon={<Issue variant="32x32_4" />}
-          onClick={openGithub}>
-          Github
-        </List.Item>
-      </List>
-    }
-  />
-  </div>
+            <List.Item
+              icon={<Drvspace7 variant="32x32_4" />}
+              onClick={handleOpenCasamentoModal}>
+              Casamento
+            </List.Item>
+            <List.Item
+              icon={<Mmsys112 variant="32x32_4" />}
+              onClick={handleOpenSpotifyModal}>
+              Playlist do casamento
+            </List.Item>
+            <List.Item
+              icon={<Packager1 variant="32x32_4" />}>
+              Lista de presentes
+            </List.Item>
+            <List.Item
+              icon={<FilePin variant="32x32_4" />}
+              onClick={handleOpenConfirmacaoModal}>
+              Confirmação de presença
+            </List.Item>
+            <List.Divider />
+            <List.Item
+              icon={<Issue variant="32x32_4" />}
+              onClick={openGithub}>
+              Github
+            </List.Item>
+          </List>
+        }
+      />
+    </div>
   );
 }
