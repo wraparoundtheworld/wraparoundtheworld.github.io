d89715048fc49fb03b6d8aa7e959962daed9733c
henriquespecian <henrique_specian@hotmail.com>
2023-10-20 17:12:07 -0300

    fix: trying to make images work

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
   images: {
     disableStaticImages: true,
   },
-  basePath: isProd ? '/casamento-nextjs' : undefined,
+  basePath: isProd ? '' : undefined,
 };
 
 module.exports = withTM(withFonts(withImages(nextConfig)));
