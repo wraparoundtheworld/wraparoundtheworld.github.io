a3653de8b407d2493ffd3a607c7e011a7cc62bc8
wraparoundtheworld <robotoeuf@gmail.com>
2024-07-05 23:44:48 +0900

    measurementid

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
-import './style.css';
+import "./style.css";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }) {
-  return   <>
-  <GoogleAnalytics />
-  <Component {...pageProps} />
-</>
+  return (
+    <>
+      <GoogleAnalytics trackPageVies gaMeasurementId="G-HDLSD7T305" debugMode />
+      <Component {...pageProps} />
+    </>
+  );
 }
