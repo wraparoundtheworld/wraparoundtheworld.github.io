a179468b4fc94c35ec08c80da669d9146eadd293
wraparoundtheworld <robotoeuf@gmail.com>
2024-07-06 17:00:42 +0900

    update again

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import { GoogleAnalytics } from 'nextjs-google-analytics';
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GoogleAnalytics trackingId={process.env.GOOGLE_ANALYTICS_ID} />
+      <GoogleAnalytics trackingId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} />
       <Component {...pageProps} />
     </>
   );
