7e4fb2c18edeb13cf7c57269c6e0a8828fdf1d47
wraparoundtheworld <robotoeuf@gmail.com>
2024-07-06 19:17:10 +0900

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,29 @@
 import './style.css';
-import { GoogleAnalytics } from "nextjs-google-analytics";
+import Script from 'next/script'
 
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-     <GoogleAnalytics strategy="lazyOnload" />
-      {/* <GoogleAnalytics trackingId={process.env.GOOGLE_ANALYTICS_ID} /> */}
+    {/* Global Site Tag (gtag.js) - Google Analytics */}
+    <Script
+      strategy="afterInteractive"
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+    />
+    <Script
+      id="gtag-init"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+        `,
+      }}
+    />
       <Component {...pageProps} />
     </>
   );
