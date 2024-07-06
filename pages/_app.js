386226e7497b8a20ecd9bda05eda8a77019cff9b
wraparoundtheworld <robotoeuf@gmail.com>
2024-07-06 17:43:47 +0900

    give up

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import './style.css';
-import { GoogleAnalytics } from 'nextjs-google-analytics';
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GoogleAnalytics trackingId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} />
+     <GoogleAnalytics strategy="lazyOnload" />
+      {/* <GoogleAnalytics trackingId={process.env.GOOGLE_ANALYTICS_ID} /> */}
       <Component {...pageProps} />
     </>
   );
