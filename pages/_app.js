6b8281b2c302030b8473c1b10fbff7603936e41d
wraparoundtheworld <robotoeuf@gmail.com>
2024-07-06 20:15:16 +0900

    !!

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }) {
     {/* Global Site Tag (gtag.js) - Google Analytics */}
     <Script
       strategy="afterInteractive"
-      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+      src={`https://www.googletagmanager.com/gtag/js?id="${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}"`}
     />
     <Script
       id="gtag-init"
